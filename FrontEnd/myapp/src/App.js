f2aa37a634f3f498e90cf1c0aec6f89b6f1fee6e
JuanDuran0307 <juanalexanderquinchoa@gmail.com>
2023-10-03 09:17:25 -0500

    ...

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -1,56 +1,14 @@
 
 import './App.css';
-
-import React, { useState } from 'react';
-
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLogin = () => {
-    // Aquí puedes agregar la lógica para verificar las credenciales del usuario
-    // y realizar la autenticación.
-    // Por ahora, solo mostraremos los valores en la consola.
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
-
+import Login from './components/login'
+function App() {
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
+    <div className="App">
+      <Login>
+
+      </Login>
     </div>
   );
-};
+}
 
-export default Login;
+export default App;
---
+++ FrontEnd/myapp/src/components/login.js
@@ -0,0 +1,56 @@
+
+import '../styles/login.css';
+
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica para verificar las credenciales del usuario
+    // y realizar la autenticación.
+    // Por ahora, solo mostraremos los valores en la consola.
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
