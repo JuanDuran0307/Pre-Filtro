186f3ba2c4bbec77249d8ef190a03c929919360e
JuanDuran0307 <juanalexanderquinchoa@gmail.com>
2023-10-05 09:20:01 -0500

    ...

--- FrontEnd/myapp/src/App.js
+++ FrontEnd/myapp/src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import dashboard from './components/dashboard';
 import Login from './components/login'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <Login>
       </Login>
-      
+      <Router>
+      <Route exact path="/dashboard" component={dashboard} />
+    </Router>
     </div>
     
   );
---
+++ FrontEnd/myapp/src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <p>Bienvenido, has iniciado sesión con éxito.</p>
+      {/* Agrega contenido adicional de tu dashboard aquí */}
+    </div>
+  );
+};
+
+export default dashboard;
